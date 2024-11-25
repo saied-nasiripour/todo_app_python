ed82b2cb258aba3640071f871e50b2208fcc6673
saied-nasiripour <saied.nasiripour@gmail.com>
2024-11-25 15:23:29 +0330

    Add an Edit Feature

--- main.py
+++ main.py
@@ -2,18 +2,23 @@
 todos = []
 
 while True:
-    user_action = input("Type add or show, exit: ")
-    user_action = user_action.strip()
+    user_action = input("Type add, show, edit, or exit: ")
+    user_action = user_action.strip() # remove the whitespace from the user input.
 
     match user_action:
         case 'add':
             todo = input("Enter a todo: ")
             todos.append(todo)
         case 'show':
-            for item in todos:
+            for item in todos: # iterate over the existing todos lists.
                 print(item)
+        case 'edit':
+            number = int(input("Number of the todo to edit: "))
+            number = number - 1
+            new_todo = input("Enter new todo: ")
+            todos[number] = new_todo
         case 'exit':
-            break
+            break # break statement is executed and the while loop terminates.
         case _:
             print("Hey, you entered an unknown command.")
 
